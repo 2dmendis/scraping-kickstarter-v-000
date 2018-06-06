149a88b58ccf8a9b093e05dc65c0dee6e8b6ac8f
Dmitri Mendis <2dmendis@gmail.com>
2018-06-06 20:24:44 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,6 +3,8 @@ require 'nokogiri'
 # require libraries/modules here
 
 # projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
